1fb85738b58b03429a5401a489d5258bbfa4b61b
Carlos Chavarria <laj.chavo@gmail.com>
2026-05-28 03:28:30 +0100

    Primer commit clase 01

--- src/main/java/cr/ac/ufidelitas/class001/Recibo.java
+++ src/main/java/cr/ac/ufidelitas/class001/Recibo.java
@@ -9,5 +9,49 @@ package cr.ac.ufidelitas.class001;
  * @author lajch
  */
 public class Recibo {
+  // Atributos
+    private int numeroRecibo;
+    private String nombre;
+    private float monto;
+
+    public Recibo(int numeroRecibo, String nombre, float monto) {
+        this.numeroRecibo = numeroRecibo;
+        this.nombre = nombre;
+        this.monto = monto;
+    }
+
+    public int getNumeroRecibo() {
+        return numeroRecibo;
+    }
+
+    public void setNumeroRecibo(int numeroRecibo) {
+        this.numeroRecibo = numeroRecibo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+        
+        
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Recibo{" + "numeroRecibo=" + numeroRecibo + ", nombre=" + nombre + ", monto=" + monto + '}';
+    }
+
+   
     
 }
